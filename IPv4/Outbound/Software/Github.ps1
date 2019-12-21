b28040d5fe668e9ae907ee139962d71b52ecadb9
metablaster <zebal@protonmail.ch>
2019-12-21 18:54:10 +0100

    add git rules

---
+++ IPv4/Outbound/Software/Github.ps1
@@ -0,0 +1,96 @@
+
+<#
+MIT License
+
+Copyright (c) 2019 metablaster zebal@protonmail.ch
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+#
+# Import global variables
+#
+. "$PSScriptRoot\..\..\..\Modules\GlobalVariables.ps1"
+
+# Ask user if he wants to load these rules
+if (!(RunThis)) { exit }
+
+#
+# Setup local variables:
+#
+$Group = "Software - Github"
+$Interface = "Wired, Wireless"
+$Profile = "Private, Public"
+$Direction = "Outbound"
+
+#
+# Git and Git Desktop installation directories
+#
+$GitRoot = "%ProgramFiles%\Git"
+$GithubRoot = "%SystemDrive%\Users\$User\AppData\Local\GitHubDesktop\app-2.2.3"
+
+# First remove all existing rules matching group
+Remove-NetFirewallRule -PolicyStore $PolicyStore -Group $Group -Direction $Direction -ErrorAction SilentlyContinue
+
+#
+# Rules for git and Github desktop
+#
+
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "Git - curl" -Service Any -Program "$GitRoot\mingw64\bin\curl.exe" `
+-PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 443 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description "curl download tool"
+
+# TODO: unsure if it's 443 or 80
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "Git - git" -Service Any -Program "$GitRoot\mingw64\bin\git.exe" `
+-PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 443 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description ""
+
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "Git - remote-https" -Service Any -Program "$GitRoot\mingw64\libexec\git-core\git-remote-https.exe" `
+-PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 443 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description "git HTTPS acces (https cloning)"
+
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "Git - ssh" -Service Any -Program "$GitRoot\usr\bin\ssh.exe" `
+-PolicyStore $PolicyStore -Enabled False -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 22 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description "git SSH acces"
+
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "GitHub Desktop - App" -Service Any -Program "$GithubRoot\GitHubDesktop.exe" `
+-PolicyStore $PolicyStore -Enabled False -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 443 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description ""
+
+New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+-DisplayName "GitHub Desktop - remote-https" -Service Any -Program "$GithubRoot\resources\app\git\mingw64\bin\git-remote-https.exe" `
+-PolicyStore $PolicyStore -Enabled False -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+-Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 443 `
+-LocalUser $User -LocalOnlyMapping $false -LooseSourceMapping $false `
+-Description "cloning repos"
