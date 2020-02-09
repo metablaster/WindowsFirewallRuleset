85b3f36d54da316025881ae5a2495885fdd880ab
metablaster <zebal@protonmail.ch>
2020-02-09 01:48:51 +0100

    rules for filezilla, java and intel

---
+++ Rules/IPv4/Outbound/Software/Java.ps1
@@ -0,0 +1,98 @@
+
+<#
+MIT License
+
+Copyright (c) 2019, 2020 metablaster zebal@protonmail.ch
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+# Test Powershell version required for this project
+Import-Module -Name $PSScriptRoot\..\..\..\..\Modules\System
+Test-SystemRequirements $VersionCheck
+
+# Includes
+. $PSScriptRoot\..\DirectionSetup.ps1
+. $PSScriptRoot\..\..\IPSetup.ps1
+Import-Module -Name $PSScriptRoot\..\..\..\..\Modules\UserInfo
+Import-Module -Name $PSScriptRoot\..\..\..\..\Modules\ProgramInfo
+Import-Module -Name $PSScriptRoot\..\..\..\..\Modules\FirewallModule
+
+#
+# Setup local variables:
+#
+$Group = "Software - Java"
+$Profile = "Private, Public"
+
+# Ask user if he wants to load these rules
+Update-Context $IPVersion $Direction $Group
+if (!(Approve-Execute)) { exit }
+
+#
+# Java installation directories
+#
+$JavaRuntimeRoot = "%ProgramFiles%\Java\jre7\bin"
+$JavaPluginRoot = "%ProgramFiles% (x86)\Java\jre1.8.0_45\bin"
+$JavaUpdateRoot = "%ProgramFiles% (x86)\Common Files\Java\Java Update"
+
+# First remove all existing rules matching group
+Remove-NetFirewallRule -PolicyStore $PolicyStore -Group $Group -Direction $Direction -ErrorAction SilentlyContinue
+
+#
+# Rules for Java
+#
+
+# Test if installation exists on system
+if ((Test-Installation "JavaRuntime" ([ref]$JavaRuntimeRoot)) -or $Force)
+{
+    $Program = "$JavaRuntimeRoot\java.exe"
+    Test-File $Program
+    New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+    -DisplayName "Runa java applets" -Service Any -Program $Program `
+    -PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+    -Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 80, 443 `
+    -LocalUser $UserAccountsSDDL `
+    -Description "Runa java applets" | Format-Output
+}
+
+# Test if installation exists on system
+if ((Test-Installation "JavaUpdate" ([ref]$JavaUpdateRoot)) -or $Force)
+{
+    $Program = "$JavaUpdateRoot\jucheck.exe"
+    Test-File $Program
+    New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+    -DisplayName "Update java" -Service Any -Program $Program `
+    -PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+    -Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 80, 443 `
+    -LocalUser $UserAccountsSDDL `
+    -Description "Update java software" | Format-Output
+}
+
+# Test if installation exists on system
+if ((Test-Installation "JavaPlugin" ([ref]$JavaPluginRoot)) -or $Force)
+{
+    $Program = "$JavaPluginRoot\jp2launcher.exe"
+    Test-File $Program
+    New-NetFirewallRule -Confirm:$Execute -Whatif:$Debug -ErrorAction $OnError -Platform $Platform `
+    -DisplayName "Java Plugin HTTP" -Service Any -Program $Program `
+    -PolicyStore $PolicyStore -Enabled True -Action Allow -Group $Group -Profile $Profile -InterfaceType $Interface `
+    -Direction $Direction -Protocol TCP -LocalAddress Any -RemoteAddress Internet4 -LocalPort Any -RemotePort 80 `
+    -LocalUser $UserAccountsSDDL `
+    -Description "Runa java applets" | Format-Output
+}
