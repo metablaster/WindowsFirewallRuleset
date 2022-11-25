7fffd3bd0fd569b42d51a3d35fd4f16db55c726d
metablaster <44481081+metablaster@users.noreply.github.com>
2022-11-26 00:35:34 +0100

    add PolicyFileEditor module to improve scripts

---
+++ Modules/PolicyFileEditor/build.psake.ps1
@@ -0,0 +1,98 @@
+#requires -Version 3.0
+
+Task default -depends Build, Sign
+
+Properties {
+	$source = $psake.build_script_dir
+	$buildTarget = "$home\Documents\WindowsPowerShell\Modules\PolicyFileEditor"
+	$signerCertThumbprint = '572DD922FB11FD90D47EE466EDF4191019FB19FA'
+	$signerTimestampUrl = 'http://timestamp.digicert.com'
+
+	$filesToExclude = @(
+		'README.md'
+		'*.Tests.ps1'
+		'build.psake.ps1'
+	)
+}
+
+Task Test {
+	$result = Invoke-Pester -Path $source -PassThru
+	$failed = $result.FailedCount
+
+	if ($failed -gt 0)
+	{
+		throw "$failed unit tests failed; build aborting."
+	}
+}
+
+Task Build -depends Test {
+	function Get-RelativePath
+	{
+		param ( [string] $Path, [string] $RelativeTo )
+		return $Path -replace "^$([regex]::Escape($RelativeTo))\\?"
+	}
+
+	if (Test-Path -Path $buildTarget -PathType Container)
+	{
+		Remove-Item -Path $buildTarget -Recurse -Force -ErrorAction Stop
+	}
+
+	$null = New-Item -Path $buildTarget -ItemType Directory -ErrorAction Stop
+
+	Get-ChildItem -Path $source\* -File -Recurse -ErrorAction Stop |
+	where { $file = $_; -not ($filesToExclude | where { $file.Name -like $_ }) } |
+	foreach {
+		$sourceFile = $_
+		$relativePath = Get-RelativePath -Path $sourceFile.FullName -RelativeTo $source
+		$targetPath = Join-Path $buildTarget $relativePath
+		$parent = Split-Path $targetPath -Parent
+
+		if (-not (Test-Path -LiteralPath $parent -PathType Container))
+		{
+			$null = New-Item -Path $parent -ItemType Directory -ErrorAction Stop
+		}
+
+		Copy-Item -LiteralPath $sourceFile.FullName -Destination $targetPath -ErrorAction Stop
+	}
+}
+
+Task Sign {
+	if (-not $signerCertThumbprint)
+	{
+		throw 'Sign task cannot run without a value in the signerCertThumbprint property.'
+	}
+
+	$paths = @(
+		"Cert:\CurrentUser\My\$signerCertThumbprint"
+		"Cert:\LocalMachine\My\$signerCertThumbprint"
+	)
+
+	$cert = Get-ChildItem -Path $paths |
+	Where-Object { $_.PrivateKey -is [System.Security.Cryptography.RSACryptoServiceProvider] } |
+	Select-Object -First 1
+
+	if ($cert -eq $null)
+	{
+		throw "Code signing certificate with thumbprint '$signerCertThumbprint' was not found, or did not have a usable private key."
+	}
+
+	$properties = @(
+		@{ Label = 'Name'; Expression = { Split-Path -Path $_.Path -Leaf } }
+		'Status'
+		@{ Label = 'SignerCertificate'; Expression = { $_.SignerCertificate.Thumbprint } }
+		@{ Label = 'TimeStamperCertificate'; Expression = { $_.TimeStamperCertificate.Thumbprint } }
+	)
+
+	$splat = @{
+		Certificate = $cert
+		IncludeChain = 'All'
+		Force = $true
+		HashAlgorithm = 'SHA256'
+	}
+
+	if ($signerTimestampUrl) { $splat['TimestampServer'] = $signerTimestampUrl }
+
+	Get-ChildItem -Path $buildTarget -Recurse -File -Include *.ps1, *.psm1, *.psd1 |
+	Set-AuthenticodeSignature @splat -ErrorAction Stop |
+	Format-Table -Property $properties -AutoSize
+}
--- Scripts/Security/Set-ATP.ps1
+++ Scripts/Security/Set-ATP.ps1
@@ -72,6 +72,9 @@ https://github.com/metablaster/WindowsFirewallRuleset/tree/master/Scripts
 
 .LINK
 https://docs.microsoft.com/en-us/powershell/module/defender/set-mppreference
+
+.LINK
+https://docs.microsoft.com/en-us/graph/api/resources/intune-deviceconfig-defendercloudblockleveltype
 #>
 
 #Requires -Version 5.1
@@ -99,33 +102,189 @@ $Deny = "Abort operation, no ASR rules will be deployed"
 if (!(Approve-Execute -Accept $Accept -Deny $Deny -Force:$Force)) { exit }
 #endregion
 
-if ($PSCmdlet.ShouldProcess("Microsoft Defender Antivirus", "Deploy recommended settings"))
+if ($PSCmdlet.ShouldProcess("Microsoft Defender Antivirus", "Configure Advanced Thread Protection"))
 {
+	# ADVANCED THREAT PROTECTION
+
+	$PolicyPath = "$env:WinDir\System32\GroupPolicy\Machine\Registry.pol"
+	# GPO: Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\MAPS
+
+	#
 	# Block at first sight (MAPS)
-	Set-MpPreference -SubmitSamplesConsent 3 -MAPSReporting Basic -DisableBlockAtFirstSeen $false `
-		-DisableIOAVProtection $false -DisableRealtimeMonitoring $false -CimSession $CimServer
+	#
 
+	Write-Information -MessageData "[$ThisScript] Join Microsoft MAPS"
+	# Join Microsoft MAPS
+	# item: decimal: 0 => Disabled
+	# item: decimal: 1 => Basic MAPS
+	# item: decimal: 2 => Advanced MAPS
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Spynet"
+	$ValueName = "SpynetReporting"
+	$Value = 2
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Send file samples when further analysis is required"
+	# Send file samples when further analysis is required (Send safe samples)
+	# item: decimal: 0 => Always prompt
+	# item: decimal: 1 => Send safe samples
+	# item: decimal: 2 => Never send
+	# item: decimal: 3 => Send all samples
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Spynet"
+	$ValueName = "SubmitSamplesConsent"
+	$Value = 1
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName "SubmitSamplesConsent" -Data 1 -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Configure the 'Block at First Sight' feature"
+	# Configure the "Block at First Sight" feature (Enabled)
+	# NOTE: Depends on:
+	# "Join Microsoft MAPS"
+	# "Send file samples when further analysis is required (option 1 or 3)"
+	# Scan all downloaded files and attachments
+	# "Real time protection - do not enable 'turn off real time protection"
+	# Enabled Value: decimal: 0
+	# Disabled Value: decimal: 1
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Spynet"
+	$ValueName = "DisableBlockAtFirstSeen"
+	$Value = 0
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Scan all downloaded files and attachments"
+	# GPO: Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\Real-time Protection
+	# Scan all downloaded files and attachments (Enabled)
+	# Enabled Value: decimal: 0
+	# Disabled Value: decimal: 1
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Real-Time Protection"
+	$ValueName = "DisableIOAVProtection"
+	$Value = 0
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Turn of realtime protection"
+	# Turn off real-time protection (Disabled)
+	# Enabled Value: decimal: 1
+	# Disabled Value: decimal: 0
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Real-Time Protection"
+	$ValueName = "DisableRealtimeMonitoring"
+	$Value = 0
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	#
 	# Cloud protection
-	# https://docs.microsoft.com/en-us/graph/api/resources/intune-deviceconfig-defendercloudblockleveltype
-	Set-MpPreference -CloudExtendedTimeout 50 -CloudBlockLevel high -EnableFileHashComputation $true -CimSession $CimServer
+	#
 
+	Write-Information -MessageData "[$ThisScript] Enable file hash computation feature"
+	# GPO: Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\mpengine
+	# Enable file hash computation feature (Optional)
+	# Enabled Value: decimal: 1
+	# Disabled Value: decimal: 0
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\MpEngine"
+	$ValueName = "EnableFileHashComputation"
+	$Value = 1
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Configure extended cloud check"
+	# Configure extended cloud check (50 max)
+	# This feature allows Microsoft Defender Antivirus to block a suspicious file for up to X seconds, and scan it in the cloud to make sure it´s safe.
+	# NOTE: This feature depends on three other MAPS settings:
+	# 1. "Configure the ´Block at First Sight´ feature
+	# 2. "Join Microsoft MAPS";
+	# 3. "Send file samples when further analysis is required" all need to be enabled.
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\MpEngine"
+	$ValueName = "MpBafsExtendedTimeout"
+	$Value = 50
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Select cloud protection level"
+	# Select cloud protection level (High blocking level)
+	# NOTE: This feature requires the "Join Microsoft MAPS"
+	# item: decimal: 0 => Default blocking level
+	# item: decimal: 1 => Moderate blocking level
+	# item: decimal: 2 => High blocking level
+	# item: decimal: 4 => High+ blocking level
+	# item: decimal: 6 => Zero tolerance blocking level
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\MpEngine"
+	$ValueName = "MpCloudBlockLevel"
+	$Value = 2
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+
+	#
+	# Attack Surface Reduction
+	#
+
+	# Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\Microsoft Defender Exploit Guard
+	# Configure Attack Surface Reduction rules
+	# Set-ASR
+
+	#
+	# Network protection
+	#
+
+	Write-Information -MessageData "[$ThisScript] Prevent users and apps from accessing dangerous websites"
+	# GPO: Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\Microsoft Defender Exploit Guard\Network Protection
+	# Prevent users and apps from accessing dangerous websites (Block)
+	# item: decimal: 0 => Disable (Default)
+	# item: decimal: 1 => Block
+	# item: decimal: 2 => Audit Mode
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Windows Defender Exploit Guard\Network Protection"
+	$ValueName = "EnableNetworkProtection"
+	$Value = 1
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	#
 	# Scan
-	Set-MpPreference -ScanAvgCPULoadFactor 60 -ScanScheduleDay Sunday -ScanScheduleTime 720 `
-		-CheckForSignaturesBeforeRunningScan $true -ScanParameters FullScan `
-		-DisableCatchupFullScan $false `
-		-DisableCatchupQuickScan $false `
-		-DisableRestorePoint $false `
-		-DisableScriptScanning $false `
-		-DisableArchiveScanning $false `
-		-DisableEmailScanning $true `
-		-DisableRemovableDriveScanning $true `
-		-DisableScanningMappedNetworkDrivesForFullScan $true `
-		-DisableScanningNetworkFiles $true `
-		-CimSession $CimServer
+	#
 
-	# Other
-	Set-MpPreference -DisableBehaviorMonitoring $false -CimSession $CimServer
-	Set-MpPreference -EnableNetworkProtection Enabled -CimSession $CimServer
+	Write-Information -MessageData "[$ThisScript] Specify the maximum percentage of CPU utilization during a scan"
+	# GPO: Computer configuration\Administrative templates\Windows Components\Microsoft Defender Antivirus\Scan
+	# Specify the maximum percentage of CPU utilization during a scan (50%, Optional)
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Scan"
+	$ValueName = "AvgCPULoadFactor"
+	$Value = 50
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	Write-Information -MessageData "[$ThisScript] Check for the latest virus and spyware security intelligence before running a scheduled scan"
+	# Check for the latest virus and spyware security intelligence before running a scheduled scan (Optional)
+	# Enabled Value: decimal: 1
+	# Disabled Value: decimal: 0
+	$RegistryPath = "Software\Policies\Microsoft\Windows Defender\Scan"
+	$ValueName = "CheckForSignaturesBeforeRunningScan"
+	$Value = 1
+	$ValueKind = [Microsoft.Win32.RegistryValueKind]::DWord
+	Set-PolicyFileEntry -Path $PolicyPath -Key $RegistryPath -ValueName $ValueName -Data $Value -Type $ValueKind
+
+	# Update changes done to registry
+	Invoke-Process gpupdate.exe -NoNewWindow -ArgumentList "/target:computer"
+
+	if ($false)
+	{
+		# TODO: Not implemented
+		Set-MpPreference -ScanScheduleDay Sunday -ScanScheduleTime 720 `
+			-ScanParameters FullScan `
+			-DisableCatchupFullScan $false `
+			-DisableCatchupQuickScan $false `
+			-DisableRestorePoint $false `
+			-DisableScriptScanning $false `
+			-DisableArchiveScanning $false `
+			-DisableEmailScanning $true `
+			-DisableRemovableDriveScanning $true `
+			-DisableScanningMappedNetworkDrivesForFullScan $true `
+			-DisableScanningNetworkFiles $true `
+			-CimSession $CimServer
+
+		# Other
+		Set-MpPreference -DisableBehaviorMonitoring $false -CimSession $CimServer
+	}
 }
 
 Update-Log
